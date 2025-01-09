9a1d7f4c2d9b68e23afd66414f6f0739ae118510
omkargwagholikar <omkargwagholikar@gmail.com>
2025-01-09 16:53:19 +0530

    Sending user type with tokens

--- base/api/serializers.py
+++ base/api/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from ..models import *
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        custom_user = CustomUser.objects.get(user=user)
 
         token['user_name'] = user.username
-
+        token['is_judge'] = custom_user.is_judge
+        token['is_player'] = custom_user.is_player
+        
         return token
