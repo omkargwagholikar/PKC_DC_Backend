9eb450b715b542a8eb9dec31904c2970091caf16
omkargwagholikar <omkargwagholikar@gmail.com>
2025-01-20 19:51:45 +0530

    Registered models, created superuser

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(CustomUser)
+admin.site.register(Question)
+admin.site.register(UploadedFile)
+admin.site.register(UserSubmission)
+admin.site.register(Judgment)
+
